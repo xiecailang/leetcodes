b99205d892402c6946b11eae3c8db19948f6e388
xiecailang <cailangxie@sina.com>
2018-09-16 10:45:58 +0800

    123

---
+++ src/longest_sub_string.py
@@ -0,0 +1,10 @@
+#最长公共子序列
+s1, s2 = input(), input()
+dp = [[0 for i in range(len(s1) + 1)] for j in range(len(s1) + 1)]
+for i in range(1, len(s1) + 1):
+    for j in range(1, len(s2) + 1):
+        if s1[i-1] == s2[j-1]:
+            dp[i][j] = dp[i - 1][j - 1] + 1
+        else:
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+print(dp[len(s1)][len(s2)])
